f56e7d2a028ba3335b7e7d62440b8ff5c4491056
crccheck <c@crccheck.com>
2015-01-31 12:23:41 -0600

    add initial proof of concept test

--- test_obj_update.py
+++ test_obj_update.py
@@ -1,8 +1,28 @@
+from __future__ import unicode_literals
+
 from django.test import TestCase
 
 from dummy.models import Foo
 
+from obj_update import update
+
 
 class RenamemeTest(TestCase):
-    def test_tests_run(self):
-        self.assertEqual(Foo.objects.count(), 0)
+    def test_can_update_fields(self):
+        # setup
+        new_val = 'hello2'
+        foo = Foo.objects.create(text='hello')
+
+        with self.assertNumQueries(1):
+            update(foo, {'text': 'hello2'})
+        self.assertEqual(Foo.objects.get(pk=foo.pk).text, new_val)
+
+    def test_no_changes_mean_no_queries(self):
+        # setup
+        foo = Foo.objects.create(text='hello')
+
+        with self.assertNumQueries(0):
+            update(foo, {'text': b'hello'})
+
+        with self.assertNumQueries(0):
+            update(foo, {'text': u'hello'})
