f891cd8d2c917635115bd2c7e2c605cc31c1c60d
crccheck <c@crccheck.com>
2015-05-30 10:40:21 -0500

    move log formatting up one level

--- obj_update.py
+++ obj_update.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from operator import itemgetter
 import logging
 import sys
 
@@ -24,9 +25,18 @@ def set_field(obj, field_name, value):
         setattr(obj, field_name, value)
         if not hasattr(obj, '_is_dirty'):
             obj._is_dirty = []
-            obj._dirty_fields = []
-        obj._is_dirty.append('[%s %s->%s]' % (field_name, old_repr, new_repr))
-        obj._dirty_fields.append(field_name)
+        obj._is_dirty.append(dict(
+            field_name=field_name,
+            old_value=old_repr,
+            new_value=new_repr,
+        ))
+
+
+def human_log_formatter(dirty_data):
+    return ''.join(
+        '[{field_name} {old_value}->{new_value}]'
+        .format(**x) for x in dirty_data
+    )
 
 
 def update(obj, data):
@@ -37,9 +47,11 @@ def update(obj, data):
     """
     for field_name, value in data.items():
         set_field(obj, field_name, value)
-    if getattr(obj, '_is_dirty', None):
-        logger.debug(u''.join(obj._is_dirty))
-        obj.save(update_fields=obj._dirty_fields)
-        del obj._is_dirty
-        del obj._dirty_fields
+    dirty_data = getattr(obj, '_is_dirty', None)
+    if dirty_data:
+        # WISHLIST ability to also output json events
+        logger.debug(human_log_formatter(dirty_data))
+        update_fields = map(itemgetter('field_name'), dirty_data)
+        obj.save(update_fields=update_fields)
+        delattr(obj, '_is_dirty')
         return True
